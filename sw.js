a3351cbfde979b458bfc40146f54a4ba01a1eab3
davissonednei <davisson.ednei@live.com>
2026-02-02 09:06:44 -0300

    feat: melhora verificacao QR Code com dados completos para comparacao

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    SERVICE WORKER - FUNCIONALIDADE OFFLINE
    ======================================== */
 
-const CACHE_NAME = 'checklist-viaturas-v11';
+const CACHE_NAME = 'checklist-viaturas-v12';
 const urlsToCache = [
     '/',
     '/index.html',
